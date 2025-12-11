1fa35201e413c9d6aa789ec9f9edaa11d51e5a61
tannnts02080-stack <tannnts02080@gmail.com>
2025-12-11 19:03:07 +0700

    ci: install matching chromedriver & update BaseTest for CI

--- src/test/java/app/BaseTest.java
+++ src/test/java/app/BaseTest.java
@@ -1,21 +1,25 @@
 package app;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.WebDriver;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 
 public class BaseTest {
-
     protected WebDriver driver;
 
     @BeforeMethod
     public void setUp() {
-        WebDriverManager.chromedriver().setup();
+        // Use system chromedriver installed by workflow
+        System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
 
         ChromeOptions options = new ChromeOptions();
+
+        // Point to system chrome binary if needed (google-chrome or chromium-browser)
+        // options.setBinary("/usr/bin/google-chrome"); // uncomment if required
+        // or options.setBinary("/usr/bin/chromium-browser");
+
         options.addArguments("--headless=new");
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-dev-shm-usage");
@@ -28,8 +32,6 @@ public class BaseTest {
 
     @AfterMethod
     public void tearDown() {
-        if (driver != null) {
-            driver.quit();
-        }
+        if (driver != null) driver.quit();
     }
 }
